b1f5e60af58cd8b951a01e1235517cfbcb73cb48
Gabriel Santos <gabrielsmdev@gmail.com>
2023-09-03 20:39:17 -0300

    added orders dashboard structure and styles

---
+++ src/components/OrdersDashboard.vue
@@ -0,0 +1,99 @@
+<template>
+  <div id="burger-table">
+    <div>
+      <div id="burger-table-heading">
+        <div class="order-id">#:</div>
+        <div>Cliente:</div>
+        <div>Pão:</div>
+        <div>Carne:</div>
+        <div>Opcionais:</div>
+        <div>Ações</div>
+      </div>
+    </div>
+    <div id="burger-table-rows">
+      <div class="burger-table-row">
+        <div class="order-number">1</div>
+        <div>Gabriel</div>
+        <div>Integral</div>
+        <div>Alcatra</div>
+        <div>
+          <ul>
+            <li>Salame</li>
+            <li>Tomate</li>
+          </ul>
+        </div>
+        <div>
+          <select name="status" class="status">
+            <option value="">Selecione o status do pedido...</option>
+          </select>
+          <button class="delete-btn">Cancelar pedido</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "OrdersDashboard",
+};
+</script>
+
+<style scoped>
+#burger-table {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+#burger-table-heading,
+#burger-table-rows,
+.burger-table-row {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+#burger-table-heading {
+  font-weight: bold;
+  padding: 12px;
+  border-bottom: 3px solid #333;
+}
+
+#burger-table-heading div,
+.burger-table-row div {
+  width: 19%;
+  display: flex;
+}
+
+.burger-table-row {
+  width: 100%;
+  padding: 12px;
+  border-bottom: 1px solid #333;
+}
+
+#burger-table-heading .order-id,
+.burger-table-row .order-number {
+  width: 5%;
+}
+
+select {
+  padding: 12px 6px;
+  margin-right: 12px;
+  width: 50%;
+}
+
+.delete-btn {
+  background-color: #222;
+  padding: 5px;
+  color: #fcba0f;
+  font-weight: bold;
+  border: 2px solid #222;
+  margin: 0 auto;
+  cursor: pointer;
+  transition: 0.5s;
+}
+
+.delete-btn:hover {
+  background-color: transparent;
+  color: #222;
+}
+</style>
--- src/views/OrdersView.vue
+++ src/views/OrdersView.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="main-container">
-    <h1>ORDERS</h1>
+    <h1>Gerenciar pedidos</h1>
+    <OrdersDashboard />
   </div>
 </template>
+
+<script lang="ts">
+import OrdersDashboard from "@/components/OrdersDashboard.vue";
+
+export default {
+  name: "OrdersView",
+  components: { OrdersDashboard },
+};
+</script>
+
+<style scoped></style>
